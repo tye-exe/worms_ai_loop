862006af67a79c46ee481f232ba12df13d8824f5
tye-exe <tye@mailbox.org>
2025-09-01 15:08:42 +0100

    Added window information caching
    This makes the debug GUI significantly less of a hastle to use.

--- src/analyser/mod.rs
+++ src/analyser/mod.rs
@@ -1,15 +1,14 @@
-use super::window::{Found, get_windows};
+mod win_info;
+
+use super::window::get_windows;
 use crate::attach;
 use eframe::{App, EventLoopBuilderHook};
-use egui::{CentralPanel, Color32, Id, Layout, TopBottomPanel};
+use egui::{CentralPanel, Id, Layout, TopBottomPanel};
 use std::{thread, time::Duration};
 use windows::{
     Win32::{
         Foundation::{HWND, POINT},
-        UI::WindowsAndMessaging::{
-            FindWindowA, GET_CLASS_LONG_INDEX, GetClassLongA, GetClassNameA, GetCursorPos,
-            GetWindowInfo, GetWindowTextA, GetWindowTextW, RealGetWindowClassA, WINDOWINFO,
-        },
+        UI::WindowsAndMessaging::{FindWindowA, GetCursorPos, GetWindowTextA},
     },
     core::s,
 };
@@ -52,14 +51,13 @@ pub fn analyse() {
 struct MyApp {
     /// Window id of worms
     window_id: HWND,
-    id: u64,
 
     /// Only show the windows that contain the cursor.
     only_containing: bool,
     /// Show extra window information.
     show_info: bool,
 
-    cached_data: Option<Found>,
+    cached_data: Option<win_info::WinData>,
     tick: usize,
 }
 
@@ -67,21 +65,11 @@ impl MyApp {
     fn new(window_id: HWND) -> Self {
         Self {
             window_id,
-            id: 1,
             only_containing: true,
             show_info: false,
             cached_data: None,
             tick: 0,
         }
-    }
-
-    fn get_id(&mut self) -> Id {
-        let id = Id::new(self.id);
-        self.id = self.id.wrapping_add(1);
-        if self.id == 0 {
-            self.id = 1;
-        }
-        id
     }
 }
 
@@ -105,132 +93,20 @@ impl App for MyApp {
         });
 
         CentralPanel::default().show(ctx, |ui| {
-            let found = match (&self.cached_data, self.tick) {
-                (None, t) if t % 10 == 0 => &get_windows(self.window_id),
+            let window_data = match (&self.cached_data, self.tick) {
+                (None, t) if t % 10 == 0 => {
+                    &win_info::get_window_data(&get_windows(self.window_id))
+                }
                 (Some(var), ..) => var,
-                _ => &get_windows(self.window_id),
+                _ => &win_info::get_window_data(&get_windows(self.window_id)),
             };
 
             egui::ScrollArea::vertical().animated(false).show(ui, |ui| {
-                for_windows(found, 0, 0, Vec::default(), &mut |data, tree_position| {
-                    let pos = data.info.rcWindow;
-
-                    // If window contains cursor
-                    if pos.bottom > cursor_pos.y
-                        && pos.top < cursor_pos.y
-                        && pos.left < cursor_pos.x
-                        && pos.right > cursor_pos.x
-                    {
-                        ui.visuals_mut().override_text_color = Some(Color32::LIGHT_RED);
-                    } else if self.only_containing {
-                        return;
-                    }
-
-                    ui.separator();
-                    let response = ui
-                        .horizontal(|ui| {
-                            ui.add_space((tree_position.depth * 50) as f32);
-                            ui.vertical(|ui| {
-                                ui.label(format!("Name: '{}'", data.name));
-                                ui.label(format!("Text: '{}'", data.text));
-                                ui.label(format!("Path: '{:?}'", tree_position.path));
-                                // ui.label(format!(
-                                //     "Depth: {}, Index: {}",
-                                //     tree_position.depth, tree_position.index
-                                // ));
-                                //ui.label(format!("Real Class: '{}'", data.w_type));
-                                //ui.label(format!("Atom: '{}'", data.atom));
-                                if self.show_info {
-                                    ui.label(format!("Info: {:#?}", data.info));
-                                }
-                            });
-                        })
-                        .response;
-
-                    if self.only_containing {
-                        ui.scroll_to_rect(response.rect, Some(egui::Align::TOP));
-                    }
-
-                    ui.visuals_mut().override_text_color = None;
-                });
+                win_info::display(&window_data, ui, &cursor_pos, self.only_containing);
             });
         });
 
         ctx.request_repaint_after(Duration::from_millis(20));
-        //ctx.request_repaint();
         self.tick = self.tick.overflowing_add(1).0;
-    }
-}
-
-/// Data about each window.
-struct WindowData<'a> {
-    text: &'a str,
-    name: &'a str,
-    w_type: &'a str,
-    info: WINDOWINFO,
-    atom: u32,
-}
-
-struct Position {
-    pub depth: usize,
-    pub index: usize,
-    pub path: Vec<usize>,
-}
-
-/// Executes the callback for each window that has been found.
-///
-/// This is a pre-order traversal.
-fn for_windows(
-    found: &Found,
-    depth: usize,
-    index: usize,
-    path: Vec<usize>,
-    callback: &mut impl FnMut(WindowData, Position) -> (),
-) {
-    let id = found.value();
-    let mut text = [' ' as u16; 255];
-    unsafe { GetWindowTextW(id, &mut text) };
-    let text = String::from_utf16(&text).expect("Unable to get window title");
-
-    let mut name = [' ' as u8; 255];
-    unsafe { GetClassNameA(id, &mut name) };
-    let name = String::from_utf8(name.to_vec()).expect("Unable to get window name");
-
-    let mut w_type = [' ' as u8; 255];
-    unsafe { RealGetWindowClassA(id, &mut w_type) };
-    let w_type = String::from_utf8(w_type.to_vec()).expect("Unable to get window type");
-
-    let mut info = WINDOWINFO::default();
-    unsafe { GetWindowInfo(id, &mut info as *mut WINDOWINFO) };
-
-    let atom = unsafe { GetClassLongA(id, GET_CLASS_LONG_INDEX(-32)) };
-
-    callback(
-        WindowData {
-            text: text.trim(),
-            name: name.trim(),
-            w_type: w_type.trim(),
-            info,
-            atom,
-        },
-        Position {
-            depth,
-            index,
-            path: path.clone(),
-        },
-    );
-
-    for (index, child) in found.children().iter().enumerate() {
-        for_windows(
-            child,
-            depth + 1,
-            index,
-            {
-                let mut path = path.clone();
-                path.push(index);
-                path
-            },
-            callback,
-        );
     }
 }
---
+++ src/analyser/win_info.rs
@@ -0,0 +1,136 @@
+use super::super::window::Found;
+use egui::{Color32, Ui};
+use windows::Win32::{
+    Foundation::POINT,
+    UI::WindowsAndMessaging::{
+        GET_CLASS_LONG_INDEX, GetClassLongA, GetClassNameA, GetWindowInfo, GetWindowTextW,
+        RealGetWindowClassA, WINDOWINFO,
+    },
+};
+
+/// Display window information to a egui ui.
+pub(crate) fn display(data: &WinData, ui: &mut Ui, cursor_pos: &POINT, only_containing: bool) {
+    let pos = data.info.rcWindow;
+
+    // If window contains cursor
+    if pos.bottom > cursor_pos.y
+        && pos.top < cursor_pos.y
+        && pos.left < cursor_pos.x
+        && pos.right > cursor_pos.x
+    {
+        ui.visuals_mut().override_text_color = Some(Color32::LIGHT_RED);
+    } else if only_containing {
+        return;
+    }
+
+    ui.separator();
+    let response = ui
+        .horizontal(|ui| {
+            ui.add_space((data.depth * 50) as f32);
+            ui.vertical(|ui| {
+                ui.label(format!("Name: '{}'", data.name));
+                ui.label(format!("Text: '{}'", data.text));
+                ui.label(format!("Path: '{:?}'", data.path));
+                // if self.show_info {
+                //     ui.label(format!("Info: {:#?}", data.info));
+                // }
+            });
+        })
+        .response;
+
+    if only_containing {
+        ui.scroll_to_rect(response.rect, Some(egui::Align::TOP));
+    }
+
+    ui.visuals_mut().override_text_color = None;
+
+    for data in data.children.iter() {
+        display(&data, ui, cursor_pos, only_containing);
+    }
+}
+
+/// Contains information about a window
+pub(crate) struct WinData {
+    pub(crate) text: Box<str>,
+    pub(crate) name: Box<str>,
+    pub(crate) w_type: Box<str>,
+    pub(crate) info: WINDOWINFO,
+    pub(crate) atom: u32,
+
+    pub(crate) depth: usize,
+    pub(crate) path: Vec<usize>,
+
+    pub(crate) children: Vec<WinData>,
+}
+
+impl WinData {
+    pub(crate) fn new(
+        text: impl Into<Box<str>>,
+        name: impl Into<Box<str>>,
+        w_type: impl Into<Box<str>>,
+        info: WINDOWINFO,
+        atom: u32,
+        depth: usize,
+        path: Vec<usize>,
+    ) -> Self {
+        Self {
+            text: text.into(),
+            name: name.into(),
+            w_type: w_type.into(),
+            info,
+            atom,
+            children: Vec::new(),
+            depth,
+            path,
+        }
+    }
+
+    pub(crate) fn add_child(&mut self, child: WinData) {
+        self.children.push(child);
+    }
+}
+
+/// Gets the [`WinData`] for found and all child windows.
+pub(crate) fn get_window_data(found: &Found) -> WinData {
+    get_window_data_rec(found, 0, Vec::new())
+}
+
+fn get_window_data_rec(found: &Found, depth: usize, path: Vec<usize>) -> WinData {
+    let id = found.value();
+    let mut text = [' ' as u16; 255];
+    unsafe { GetWindowTextW(id, &mut text) };
+    let text = String::from_utf16(&text).expect("Unable to get window title");
+
+    let mut name = [' ' as u8; 255];
+    unsafe { GetClassNameA(id, &mut name) };
+    let name = String::from_utf8(name.to_vec()).expect("Unable to get window name");
+
+    let mut w_type = [' ' as u8; 255];
+    unsafe { RealGetWindowClassA(id, &mut w_type) };
+    let w_type = String::from_utf8(w_type.to_vec()).expect("Unable to get window type");
+
+    let mut info = WINDOWINFO::default();
+    unsafe { GetWindowInfo(id, &mut info as *mut WINDOWINFO) };
+
+    let atom = unsafe { GetClassLongA(id, GET_CLASS_LONG_INDEX(-32)) };
+
+    let mut data = WinData::new(
+        text.trim(),
+        name.trim(),
+        w_type.trim(),
+        info,
+        atom,
+        depth,
+        path.clone(),
+    );
+
+    for (index, child) in found.children().iter().enumerate() {
+        data.add_child(get_window_data_rec(child, depth + 1, {
+            let mut v = path.clone();
+            v.push(index);
+            v
+        }));
+    }
+
+    data
+}
